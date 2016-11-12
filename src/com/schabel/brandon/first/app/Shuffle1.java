9b673a570d2593f03674bafc98fde200ba1c1dc5
brandon-schabel <brandonschabel1995@gmail.com>
2016-11-12 07:05:34 -0800

    first commit

---
+++ src/com/schabel/brandon/first/app/Shuffle1.java
@@ -0,0 +1,23 @@
+package com.schabel.brandon.first.app;
+
+public class Shuffle1 {
+	public static void main(String[] args) {
+		int x = 3;
+		
+		while(x > 0) {
+			
+			if(x >  2){
+				System.out.print("a");
+			}
+			if(x == 2) {
+				System.out.print("d c");
+				x= x - 1;
+			}
+			if(x == 1 ) {
+				System.out.print("d");
+			}
+			x = x-1;
+			System.out.print("-");
+		}
+	}
+}
